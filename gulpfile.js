7cabee36a78f14cf388c414502284e5cbd805f30
Johann Dyck <contact@johanndyck.com>
2016-07-22 11:45:59 -0500

    added sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp'),
 		gutil = require('gulp-util'),
 		browserify = require('gulp-browserify'),
 		// browserify let's us import JS libraries like jQuery
+		compass = require('gulp-compass'),
+		// compass processes our SCSS
 		concat = require('gulp-concat');
 
 var jsSources = [
 	'components/scripts/scripts.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 
 gulp.task('js', function() {
@@ -14,4 +17,15 @@ gulp.task('js', function() {
 	.pipe(concat('script.js'))
 	.pipe(browserify())
 	.pipe(gulp.dest('development/js'))
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+	.pipe(compass({
+		css: 'development/css',
+		sass: 'components/sass',
+		image: 'development/images',
+		style: 'expanded'
+	}))
+		.on('error', gutil.log)
 });
