4735da2316124a14f6ac7c1e2adb3af3ee1f79c9
Johann Dyck <contact@johanndyck.com>
2016-07-23 00:07:01 -0500

    added html to watch and live refresh

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ var gulp = require('gulp'),
 		concat = require('gulp-concat');
 
 var jsSources = ['components/scripts/scripts.js'],
-		sassSources = ['components/sass/style.scss'];
+		sassSources = ['components/sass/style.scss'],
+		htmlSources = ['development/*.html'];
 
 gulp.task('js', function() {
 	gulp.src(jsSources)
@@ -38,9 +39,15 @@ gulp.task('connect', function() {
 	});
 });
 
-gulp.task('watch', function() {
-	gulp.watch(jsSources, ['js'])
-	gulp.watch('components/sass/*.scss', ['compass'])
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+	.pipe(connect.reload())
 });
 
-gulp.task('default', ['js','compass', 'watch', 'connect']);
+gulp.task('watch', function() {
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+});
+
+gulp.task('default', ['js','compass', 'watch', 'connect', 'html']);
